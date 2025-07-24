e1130de21ab40af1dd8e502d65f2d09605a51965
Valeria Mamedova <valeria.mamedova@hs-osnabrueck.de>
2025-07-24 11:31:54 +0200

    Read and write receivern

--- src/receivers/read_receiver.py
+++ src/receivers/read_receiver.py
@@ -1,2 +1,9 @@
-# base read receiver class
-pass
+class ReadReceiver:
+    def read_row(self, file_path: str):
+        raise NotImplementedError()
+
+    def read_bulk(self, file_path: str):
+        raise NotImplementedError()
+
+    def read_bigdata(self, file_path: str, chunk_size: int = 1000):
+        raise NotImplementedError()
--- src/receivers/write_receiver.py
+++ src/receivers/write_receiver.py
@@ -1,2 +1,9 @@
-# base write receiver class
-pass
+class WriteReceiver:
+    def write_row(self, file_path: str, row: list):
+        raise NotImplementedError()
+
+    def write_bulk(self, file_path: str, rows: list[list]):
+        raise NotImplementedError()
+
+    def write_bigdata(self, file_path: str, data_generator):
+        raise NotImplementedError()
