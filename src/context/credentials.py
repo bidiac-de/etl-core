62e7d2b8385d3c3adac5454e5e32b4d2be8d5cc0
Valeria Mamedova <valeria.mamedova@hs-osnabrueck.de>
2025-07-24 18:07:10 +0200

    inserted DataOperationsReceiver, fixed credentials

--- src/context/credentials.py
+++ src/context/credentials.py
@@ -1,4 +1,7 @@
-class Credentials:
+from context.context_provider import IContextProvider
+
+
+class Credentials(IContextProvider):
     def __init__(self, credentials_id: int, user: str, password: str, database: str):
         self.credentials_id = credentials_id
         self.user = user
