2ee39fe54a46517f337b70363ddcc040b696b5fa
phtrumpf <167432784+phtrumpf@users.noreply.github.com>
2025-07-28 18:03:12 +0200

    moved logging setup to correct startup

--- src/api/main.py
+++ src/api/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 
+from src.logging.logging_setup import setup_logging
+
 app = FastAPI()
 
 
 if __name__ == "__main__":
     import uvicorn
+
+    setup_logging()
 
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- src/main.py
+++
@@ -1,9 +0,0 @@
-from src.logging.logging_setup import setup_logging
-
-
-def main():
-    setup_logging()
-
-
-if __name__ == "__main__":
-    main()
