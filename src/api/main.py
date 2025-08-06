160a52292d073ec6eb39e64f1b0671dc06e8fa6a
phtrumpf <167432784+phtrumpf@users.noreply.github.com>
2025-08-06 15:12:40 +0200

    style

--- src/api/main.py
+++ src/api/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from src.logging.logging_setup import setup_logging
-from src.api.routers import schemas
-from src.api.routers import setup
+from src.api.routers import schemas, setup, jobs
 from src.api.helpers import autodiscover_components
 
 autodiscover_components("src.components")
 app = FastAPI()
 app.include_router(schemas.router)
 app.include_router(setup.router)
+app.include_router(jobs.router)
+
 
 if __name__ == "__main__":
     import uvicorn
