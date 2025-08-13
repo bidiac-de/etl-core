b5b63d6a73296eb159dd916f719620564d426a1a
chofste <116663482+chofste@users.noreply.github.com>
2025-08-13 12:26:09 +0200

    moved main application file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from src.logging.logging_setup import setup_logging
-from src.api.routers import schemas, setup, jobs, execution
-from src.api.helpers import autodiscover_components
-from src.persistance.handlers.job_handler import JobHandler
-from src.job_execution.job_execution_handler import JobExecutionHandler
 from contextlib import asynccontextmanager
 
+from .logger.logging_setup import setup_logging
+from .api.routers import schemas, setup, jobs, execution
+from .api.helpers import autodiscover_components
+from .persistance.handlers.job_handler import JobHandler
+from .job_execution.job_execution_handler import JobExecutionHandler
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -30,4 +30,4 @@ if __name__ == "__main__":
 
     setup_logging()
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
