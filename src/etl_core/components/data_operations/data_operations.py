f56091f54d2f39edb2964619ebcefe36ae175254
phtrumpf <167432784+phtrumpf@users.noreply.github.com>
2025-08-19 15:38:59 +0200

    removed/updated classes that have changed in other branches

--- src/etl_core/components/data_operations/data_operations.py
+++ src/etl_core/components/data_operations/data_operations.py
@@ -1,13 +1,14 @@
-from typing import List, Optional
-from etl_core.components.base_component import Component
+from pydantic import ConfigDict
 from abc import ABC
+from etl_core.components.base_component import Component
 
 
 class DataOperationsComponent(Component, ABC):
-    """Concrete data operations component, e.g. filter, map, transform."""
+    """
+    Concrete data operations component, e.g. filter, map, transform.
+    """
 
-    def __init__(
-        self, *args, subcomponents: Optional[List[Component]] = None, **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        self.subcomponents = subcomponents or []
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        extra="ignore",
+    )
